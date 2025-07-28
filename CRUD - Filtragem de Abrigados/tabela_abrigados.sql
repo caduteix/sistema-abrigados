24851edfc5e9ebc585b5bf04a6a8a192a62312ac
João Davi <joaodavi.fa.dantas@gmail.com>
2025-07-28 12:58:24 -0300

    ajeitanado

--- CRUD - Filtragem de Abrigados/tabela_abrigados.sql
+++ CRUD - Filtragem de Abrigados/tabela_abrigados.sql
@@ -11,13 +11,13 @@ CREATE TABLE IF NOT EXISTS pessoa_abrigada (
 
 -- Inserção de 10 abrigados
 INSERT INTO pessoa_abrigada (nome, genero, data_nascimento, documento, status, condicoes_saude) VALUES
-('João Silva', 'Masculino', '1980-03-12', '12345678900', 'abrigado', 'diabetes'),
-('Maria Oliveira', 'Feminino', '1975-05-10', '98765432100', 'abrigado', 'hipertensão'),
+('Arthur Silva', 'Masculino', '1980-03-12', '12345678900', 'abrigado', 'diabetes'),
+('Maria Oliveira', 'Feminino', '1975-05-10', '98765432100', 'abrigado', 'hipertensao'),
 ('Carlos Pereira', 'Masculino', '1968-07-21', '11122233344', 'encaminhado', 'nenhuma'),
-('Ana Lima', 'Feminino', '1985-01-22', '22233344455', 'abrigado', 'deficiência visual'),
+('Ana Lima', 'Feminino', '1985-01-22', '22233344455', 'abrigado', 'deficiencia visual'),
 ('Pedro Souza', 'Masculino', '1990-12-01', '33344455566', 'saiu', 'epilepsia'),
 ('Juliana Rocha', 'Feminino', '2000-08-30', '44455566677', 'abrigado', 'diabetes'),
 ('Rafael Duarte', 'Masculino', '1982-04-17', '55566677788', 'abrigado', 'nenhuma'),
-('Luciana Costa', 'Feminino', '1979-06-05', '66677788899', 'encaminhado', 'hipertensão'),
-('Bruno Marques', 'Masculino', '1995-11-09', '77788899900', 'saiu', 'deficiência auditiva'),
+('Luciana Costa', 'Feminino', '1979-06-05', '66677788899', 'encaminhado', 'hipertensao'),
+('Bruno Marques', 'Masculino', '1995-11-09', '77788899900', 'saiu', 'deficiencia auditiva'),
 ('Camila Nunes', 'Feminino', '1988-02-28', '88899900011', 'abrigado', 'diabetes');
